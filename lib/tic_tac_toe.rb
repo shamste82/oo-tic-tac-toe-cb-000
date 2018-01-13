086aad631b8db6db63c87015a82d56cb80226423
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-13 16:20:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,4 +105,17 @@ class TicTacToe
     return false
   end
 
+  def winner
+  combination = won?
+  if !combination
+    return nil
+  end
+  if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X"
+    return "X"
+  elsif @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
+    return "O"
+  end
+end
+
+
 end
