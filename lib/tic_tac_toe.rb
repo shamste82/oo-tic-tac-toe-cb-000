78c3614ee52bbca94bc4dcdf64f46e4233b44c3a
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-13 15:49:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,4 +57,24 @@ class TicTacToe
     end
     turn()
   end
+
+  def won?()
+    WIN_COMBINATIONS.each do |combination|
+        win_index_1 = combination[0]
+        win_index_2 = combination[1]
+        win_index_3 = combination[2]
+
+        position_1 = @board[win_index_1]
+        position_2 = @board[win_index_2]
+        position_3 = @board[win_index_3]
+
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return combination
+        end
+        if position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return combination
+        end
+    end
+    return false
+  end
 end
